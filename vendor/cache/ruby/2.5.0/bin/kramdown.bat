48ff1f59ab002d612c8310377b46b820748e05a4
Joel M <jmasselink@gmail.com>
2019-10-14 14:23:20 -0700

    removed excess Gem stuff

---
+++ vendor/cache/ruby/2.5.0/bin/kramdown.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"C:\tools\ruby25\bin\ruby.exe" "C:/code/website/cbogezi.github.io/vendor/cache/ruby/2.5.0/bin/kramdown" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"C:\tools\ruby25\bin\ruby.exe" "%~dpn0" %*
