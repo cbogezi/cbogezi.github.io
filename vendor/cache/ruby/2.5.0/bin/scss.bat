5b279ee3c13cbd4b3dca049aeebd1650b1adca13
Joel M <jmasselink@gmail.com>
2019-10-15 10:13:51 -0700

    removed vendor\cache\ gems

--- vendor/cache/ruby/2.5.0/bin/scss.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\tools\ruby25\bin\ruby.exe" "C:/code/website/cbogezi.github.io/vendor/cache/ruby/2.5.0/bin/scss" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"C:\tools\ruby25\bin\ruby.exe" "%~dpn0" %*
